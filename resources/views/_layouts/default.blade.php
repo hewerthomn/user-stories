a18a076a1ac8f44aab76bbfa4c180dc22fc2ac87
Éverton Inocêncio <hewerthomn@gmail.com>
2015-08-26 19:08:12 -0400

    added favicon, touch icons and mobile-web-apple-capable meta tags

--- resources/views/_layouts/default.blade.php
+++ resources/views/_layouts/default.blade.php
@@ -7,7 +7,12 @@
 
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
+
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    @include('_partials.meta-icons')
 
     <title>Stories {{ isset($title) ? ' | '.$title : '' }}</title>
 
