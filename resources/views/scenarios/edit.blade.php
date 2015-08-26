a461211c6155afe006628a69f9a374d3d38633b9
Éverton Inocêncio <hewerthomn@gmail.com>
2015-08-25 21:23:04 -0400

    removed panels from rows

--- resources/views/scenarios/edit.blade.php
+++ resources/views/scenarios/edit.blade.php
@@ -13,46 +13,41 @@
 </h1>
 <hr>
 
+<div class="panel panel-default">
+    <div class="panel-body">
+        <label for="project_id">Project</label>
+        <h3>
+            <small>#{{ $scenario->story->project->id }}</small>
+            <a href="{{ route('projects.show', $scenario->story->project->id) }}">
+                {{ $scenario->story->project->name }}
+            </a>
+        </h3>
+    </div>
+</div>
+
 {!! Form::model($scenario, ['method' => 'PUT', 'route' => ['scenarios.update', $scenario->id]]) !!}
 
-<div class="row">
-    <div class="col-md-6">
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <label for="story_id">Story</label>
-                <h3>
-                    <small>{{ $scenario->story->uid }}</small>
+<div class="panel panel-default">
+    <div class="panel-body">
+        <label for="story_id">Story</label>
+        <h3>
+            <small>{{ $scenario->story->uid }}</small>
 
-                    <a href="{{ route('projects.show', ['id' => $scenario->story->project->id, 'story_id' => $scenario->story->id]) }}">
-                        {{ $scenario->story->title }}
-                    </a>
-                    {!! Form::hidden('story_id', $scenario->story->id) !!}
-                </h3>
-                <p>
-                    <b>As a</b>
-                    <span>{{ $scenario->story->who }}</span>
-                    <br>
-                    <b>I want</b>
-                    <span>{{ $scenario->story->what }}</span>
-                    <br>
-                    <b>So then</b>
-                    <span>{{ $scenario->story->why }}</span>
-                </p>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-6">
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <label for="project_id">Project</label>
-                <h3>
-                    <small>#{{ $scenario->story->project->id }}</small>
-                    <a href="{{ route('projects.show', $scenario->story->project->id) }}">
-                        {{ $scenario->story->project->name }}
-                    </a>
-                </h3>
-            </div>
-        </div>
+            <a href="{{ route('projects.show', ['id' => $scenario->story->project->id, 'story_id' => $scenario->story->id]) }}">
+                {{ $scenario->story->title }}
+            </a>
+            {!! Form::hidden('story_id', $scenario->story->id) !!}
+        </h3>
+        <p>
+            <b>As a</b>
+            <span>{{ $scenario->story->who }}</span>
+            <br>
+            <b>I want</b>
+            <span>{{ $scenario->story->what }}</span>
+            <br>
+            <b>So then</b>
+            <span>{{ $scenario->story->why }}</span>
+        </p>
     </div>
 </div>
 
