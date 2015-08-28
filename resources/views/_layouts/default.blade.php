c1790e4c6d88b46b815ba8c35c2dd0a02455e7bd
Éverton Inocêncio <hewerthomn@gmail.com>
2015-08-27 21:38:59 -0400

    added link to projects

--- resources/views/_layouts/default.blade.php
+++ resources/views/_layouts/default.blade.php
@@ -39,6 +39,13 @@
           </a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-left">
+            <li>
+              <a href="{{ route('projects.index') }}">
+                Projects
+              </a>
+            </li>
+          </ul>
           <ul class="nav navbar-nav navbar-right">
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
