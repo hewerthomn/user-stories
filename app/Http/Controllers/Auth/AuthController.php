6b782d4d96e834b265f19440a65b93b65d01c12e
Éverton Inocêncio <hewerthomn@gmail.com>
2015-08-30 10:38:49 -0400

    fixed visibily of redirectTo property

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller
     |
     */
 
-    public $redirectTo = '/';
+    protected $redirectTo = '/';
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
